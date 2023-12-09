3c0a465b272dbb97cfb5166a4a7aa6147cc3dda4
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-09 18:43:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function fibonacci(num) {
 // your code here
+	 if (num <= 0) {
+        return "Invalid input. Please provide a positive integer.";
+    }
+
+    if (num === 1) {
+        return 0;
+    } else if (num === 2) {
+        return 1;
+    } else {
+        let a = 0, b = 1;
+        for (let i = 3; i <= num; i++) {
+            let temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
 }
+console.log(fibonacci(1));  // Output: 0
+console.log(fibonacci(5));  // Output: 3
 
 module.exports = fibonacci;
